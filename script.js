89b872a990e7e4f6310fd30dc803acc6d4504179
Moses Lee <leecmoses@gmail.com>
2022-04-24 10:24:35 -0400

    Reduce movements into deposit and withdrawal totals

--- script.js
+++ script.js
@@ -252,3 +252,32 @@ btnClose.addEventListener("click", (e) => {
 //   // );
 //   // console.log(movementsUI2);
 // });
+
+// PRACTICE
+// Solution #1
+// const numDeposits1000 = accounts
+//   .flatMap((acc) => acc.movements)
+//   .filter((mov) => mov >= 1000).length;
+
+// Solution #2 using reduce and prefixed ++ operator
+// const numDeposits1000 = accounts
+//   .flatMap((acc) => acc.movements)
+//   .reduce((count, cur) => {
+//     return cur >= 1000 ? ++count : count;
+//   }, 0);
+
+// console.log(numDeposits1000);
+
+// Using accumlator for sums within an object
+// const { deposits, withdrawals } = accounts
+//   .flatMap((acc) => acc.movements)
+//   .reduce(
+//     (sums, cur) => {
+//       // cur > 0 ? (sums.deposits += cur) : (sums.withdrawals += cur);
+//       sums[cur > 0 ? "deposits" : "withdrawals"] += cur; // More DRY than the line above
+//       return sums;
+//     },
+//     { deposits: 0, withdrawals: 0 }
+//   );
+
+// console.log(deposits, withdrawals);
